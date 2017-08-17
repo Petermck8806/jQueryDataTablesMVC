af297093518b822bbdab34a8c1097626765ce66b
Pete <pmckinney8806@gmail.com>
2017-08-17 13:29:15 -0500

    Correcting bundling issues.

--- jQueryDataTables/App_Start/BundleConfig.cs
+++ jQueryDataTables/App_Start/BundleConfig.cs
@@ -14,14 +14,6 @@ namespace jQueryDataTables
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
-                        "~/Scripts/DataTables/jquery.dataTables.min.js",
-                        "~/Scripts/DataTables/dataTables.bootstrap.min.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/datatables/css").Include(
-                        "~/Content/DataTables/css/dataTables.bootstrap.min.css",
-                        "~/Content/DataTables/css/responsive.bootstrap.min.css"));
-
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
@@ -31,8 +23,13 @@ namespace jQueryDataTables
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
+            "~/Scripts/DataTables/jquery.dataTables.min.js",
+            "~/Scripts/DataTables/dataTables.bootstrap.min.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/DataTables/css/dataTables.bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
